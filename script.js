97613396f3a393da2d38efa69824f4ab8c3c25c0
NickBauru-881 <nicolasantunes.mkt@gmail.com>
2026-01-09 14:42:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ function girar() {
          });
      }
 
-     const numeroWhatsApp = '5514997246268'; 
+     const numeroWhatsApp = '5514997796601'; 
      const mensagem = `Oi 👋 Girei a roleta da Hyundai Top Motors e ganhei: *${premio}*! Como resgato? 🤩`;
      const linkWhatsapp = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensagem)}`;
 
